96012999b82fe0a91941ba4cde5380cde61b8c80
Virenbar <rib.artem@gmail.com>
2021-12-08 13:53:24 +0500

    Day 8

--- AdventOfCode2021/AdventOfCode2021/Program.cs
+++ AdventOfCode2021/AdventOfCode2021/Program.cs
@@ -7,7 +7,7 @@ namespace AdventOfCode2021
 	{
 		private static void Main()
 		{
-			BaseDay Day = new Day07();
+			BaseDay Day = new Day08();
 			var (PartOne, PartTwo) = Day.Solve();
 			Console.WriteLine(PartOne);
 			Console.WriteLine(PartTwo);
