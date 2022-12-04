076edc639905c950f75c7b5935dc3d99f811cbf0
Virenbar <rib.artem@gmail.com>
2022-12-04 17:35:59 +0500

    Day 4

--- AdventOfCode2022/AdventOfCode2022/Days/Day04.cs
+++ AdventOfCode2022/AdventOfCode2022/Days/Day04.cs
@@ -1,4 +1,6 @@
-﻿namespace AdventOfCode2022.Days
+﻿using System.Text.RegularExpressions;
+
+namespace AdventOfCode2022.Days
 {
     public class Day04 : BaseDay
     {
@@ -8,38 +10,63 @@
 
         protected override string SolvePartOne()
         {
-            var R = CalculatePartOne(Lines);
+            var R = CountFullOverlaps(Lines);
             return R.ToString();
         }
 
         protected override string SolvePartTwo()
         {
-            var R = CalculatePartTwo(Lines);
+            var R = CountOverlaps(Lines);
             return R.ToString();
         }
 
         protected override bool TestPartOne()
         {
-            var R = CalculatePartOne(LinesTest);
-            return R == 0;
+            var R = CountFullOverlaps(LinesTest);
+            return R == 2;
         }
 
         protected override bool TestPartTwo()
         {
-            var R = CalculatePartTwo(LinesTest);
-            return R == 0;
-        }
-
-        private static int CalculatePartOne(List<string> _)
-        {
-            return 0;
-        }
-
-        private static int CalculatePartTwo(List<string> _)
-        {
-            return 0;
+            var R = CountOverlaps(LinesTest);
+            return R == 4;
         }
 
         #endregion Overrides
+
+        private static int CountFullOverlaps(List<string> pairs)
+        {
+            var Pairs = pairs.Select(P => new Pair(P));
+            return Pairs.Count(P => P.IsFullOverlapp());
+        }
+
+        private static int CountOverlaps(List<string> pairs)
+        {
+            var Pairs = pairs.Select(P => new Pair(P));
+            return Pairs.Count(P => P.IsOverlapp());
+        }
+
+        private class Pair
+        {
+            private static readonly Regex R = new(@"\d+");
+
+            public Pair(string pair)
+            {
+                var Numbers = R.Matches(pair).Select(M => int.Parse(M.Value)).ToList();
+                for (int i = Numbers[0]; i <= Numbers[1]; i++) { First.Add(i); }
+                for (int i = Numbers[2]; i <= Numbers[3]; i++) { Second.Add(i); }
+            }
+
+            public HashSet<int> First { get; } = new();
+            public HashSet<int> Second { get; } = new();
+
+            public bool IsFullOverlapp()
+            {
+                var Length = First.Intersect(Second).Count();
+                return Length == First.Count || Length == Second.Count;
+            }
+
+            public bool IsOverlapp() => First.Intersect(Second).Any();
+        }
     }
 }
