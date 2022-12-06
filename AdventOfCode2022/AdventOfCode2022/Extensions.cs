d53c3c2c74fe6e3a870d11379b249b86f43b3e58
Virenbar <rib.artem@gmail.com>
2022-12-06 10:51:14 +0500

    Add extension
    Move types

--- AdventOfCode2022/AdventOfCode2022/Extensions.cs
+++ AdventOfCode2022/AdventOfCode2022/Extensions.cs
@@ -2,6 +2,17 @@
 {
     public static class Extensions
     {
+        public static bool IsAllUnique(this IEnumerable<char> chars)
+        {
+            var Chars = new HashSet<char>();
+            foreach (var Char in chars)
+            {
+                if (Chars.Contains(Char)) { return false; }
+                else { Chars.Add(Char); }
+            }
+            return true;
+        }
+
         public static List<string> SplitBlocksToList(this string input) => input.Split(new[] { "\r\n\r\n", "\r\r", "\n\n" }, StringSplitOptions.None).ToList();
 
         public static List<string> SplitToList(this string input) => input.Split(new[] { "\r\n", "\r", "\n" }, StringSplitOptions.None).ToList();
