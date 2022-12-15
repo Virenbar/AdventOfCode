51f17d517b772aa847722b769a5e036391910aa8
Virenbar <rib.artem@gmail.com>
2022-12-15 14:13:36 +0500

    Day 15

--- AdventOfCode2022/AdventOfCode2022/Days/Day15.cs
+++ AdventOfCode2022/AdventOfCode2022/Days/Day15.cs
@@ -1,6 +1,8 @@
-﻿namespace AdventOfCode2022.Days
+﻿using System.Text.RegularExpressions;
+
+namespace AdventOfCode2022.Days
 {
-    public class Day15 : BaseDay
+    public partial class Day15 : BaseDay
     {
         #region Overrides
 
@@ -8,38 +10,95 @@
 
         protected override string SolvePartOne()
         {
-            var R = CalculatePartOne(Lines);
+            var R = CountEmptyPoints(Lines, 2000000);
             return R.ToString();
         }
 
         protected override string SolvePartTwo()
         {
-            var R = CalculatePartTwo(Lines);
+            var R = FindFrequency(Lines, 4000000);
             return R.ToString();
         }
 
         protected override bool TestPartOne()
         {
-            var R = CalculatePartOne(LinesTest);
-            return R == 0;
+            var R = CountEmptyPoints(LinesTest, 10);
+            return R == 26;
         }
 
         protected override bool TestPartTwo()
         {
-            var R = CalculatePartTwo(LinesTest);
-            return R == 0;
-        }
-
-        private static int CalculatePartOne(List<string> _)
-        {
-            return 0;
-        }
-
-        private static int CalculatePartTwo(List<string> _)
-        {
-            return 0;
+            var R = FindFrequency(LinesTest, 20);
+            return R == 56000011;
         }
 
         #endregion Overrides
+
+        private static int CountEmptyPoints(List<string> sensors, int row)
+        {
+            var Scan = new SensorsScan(sensors);
+            return Scan.CountEmpty(row);
+        }
+
+        private static long FindFrequency(List<string> sensors, int size)
+        {
+            var Scan = new SensorsScan(sensors);
+            return Scan.FindFrequency(size);
+        }
+
+        [GeneratedRegex("=(?<SX>.+),.+=(?<SY>.+):.+=(?<BX>.+),.+=(?<BY>.+)")]
+        private static partial Regex SensorRegex();
+
+        private class SensorsScan
+        {
+            private readonly HashSet<Point> Beacons = new();
+            private readonly List<(Point Sensor, Point Beacon, int Distance)> Data = new();
+
+            public SensorsScan(List<string> sensors)
+            {
+                foreach (var sensor in sensors)
+                {
+                    var Match = SensorRegex().Match(sensor);
+                    Point Sensor = new(int.Parse(Match.Groups["SX"].Value), int.Parse(Match.Groups["SY"].Value));
+                    Point Beacon = new(int.Parse(Match.Groups["BX"].Value), int.Parse(Match.Groups["BY"].Value));
+                    var Distance = Math.Abs(Sensor.X - Beacon.X) + Math.Abs(Sensor.Y - Beacon.Y);
+                    Beacons.Add(Beacon);
+                    Data.Add((Sensor, Beacon, Distance));
+                }
+            }
+
+            public int CountEmpty(int row)
+            {
+                var MinX = Data.Select(D => D.Sensor.X - (D.Distance - Math.Abs(row - D.Sensor.Y))).Min();
+                var MaxX = Data.Select(D => D.Sensor.X + (D.Distance - Math.Abs(row - D.Sensor.Y))).Max();
+
+                var Zone = Enumerable.Range(MinX, MaxX - MinX + 1)
+                    .Select(X => new Point(X, row))
+                    .Where(P => Data.Any(D => Math.Abs(P.X - D.Sensor.X) + Math.Abs(P.Y - D.Sensor.Y) <= D.Distance))
+                    .Where(P => !Beacons.Contains(P));
+                return Zone.Count();
+            }
+
+            public long FindFrequency(int size)
+            {
+                foreach (var X in Enumerable.Range(0, size + 1))
+                {
+                    var Ranges = Data
+                        .Select(D => (D.Sensor, YDistance: D.Distance - Math.Abs(X - D.Sensor.X)))
+                        .Where(D => D.YDistance >= 0)
+                        .Select(D => (MinY: D.Sensor.Y - D.YDistance, MaxY: D.Sensor.Y + D.YDistance))
+                        .OrderBy(D => D.MinY)
+                        .ToList();
+                    var Y = 0;
+                    foreach (var (MinY, MaxY) in Ranges)
+                    {
+                        if (MinY <= Y + 1) { Y = Math.Max(MaxY, Y); }
+                        else { return X * 4000000L + Y + 1; }
+                    }
+                }
+                return 0;
+            }
+        }
+        private record Point(int X, int Y);
     }
 }
