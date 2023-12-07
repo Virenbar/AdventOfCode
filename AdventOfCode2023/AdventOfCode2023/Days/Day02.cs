f3395fa82762f3f5b5584aa67fb2cf3a8207b444
Virenbar <rib.artem@gmail.com>
2023-12-07 16:39:34 +0500

    Day 2

--- AdventOfCode2023/AdventOfCode2023/Days/Day02.cs
+++ AdventOfCode2023/AdventOfCode2023/Days/Day02.cs
@@ -1,6 +1,8 @@
-﻿namespace AdventOfCode2023.Days
+﻿using System.Text.RegularExpressions;
+
+namespace AdventOfCode2023.Days
 {
-    public class Day02 : BaseDay
+    public partial class Day02 : BaseDay
     {
         #region Overrides
 
@@ -8,38 +10,85 @@
 
         protected override string SolvePartOne()
         {
-            var R = CalculatePartOne(Lines);
+            var R = FindGames(Lines);
             return R.ToString();
         }
 
         protected override string SolvePartTwo()
         {
-            var R = CalculatePartTwo(Lines);
+            var R = FindGamesMinimal(Lines);
             return R.ToString();
         }
 
         protected override bool TestPartOne()
         {
-            var R = CalculatePartOne(LinesTest);
-            return R == 0;
+            var R = FindGames(LinesTest);
+            return R == 8;
         }
 
         protected override bool TestPartTwo()
         {
-            var R = CalculatePartTwo(LinesTest);
-            return R == 0;
+            var R = FindGamesMinimal(LinesTest);
+            return R == 2286;
         }
 
         #endregion Overrides
 
-        private static int CalculatePartOne(List<string> _)
+        private static int FindGames(List<string> input)
         {
-            return 0;
+            var max = new GameResult(12, 13, 14);
+            var games = input.Select(ParseGame).ToList();
+            var sum = games.Where(G =>
+                G.Results.TrueForAll(R =>
+                    R.Red <= max.Red &&
+                    R.Green <= max.Green &&
+                    R.Blue <= max.Blue)
+                ).Sum(G => G.ID);
+            return sum;
         }
 
-        private static int CalculatePartTwo(List<string> _)
+        private static int FindGamesMinimal(List<string> input)
         {
-            return 0;
+            var games = input.Select(ParseGame).ToList();
+            var sum = games.Sum(G =>
+                G.Results.Max(R => R.Red) *
+                G.Results.Max(R => R.Green) *
+                G.Results.Max(R => R.Blue));
+            return sum;
         }
+
+        private static Game ParseGame(string game)
+        {
+            var items = game.Split(':', ';');
+            var id = int.Parse(RGame().Match(items[0]).Value);
+
+            var results = new List<GameResult>();
+            foreach (var item in items.Skip(1))
+            {
+                var red = int.TryParse(RRed().Match(item).Value, out var r) ? r : 0;
+                var green = int.TryParse(RGreen().Match(item).Value, out var g) ? g : 0;
+                var blue = int.TryParse(RBlue().Match(item).Value, out var b) ? b : 0;
+
+                results.Add(new GameResult(red, green, blue));
+            }
+
+            return new Game(id, results);
+        }
+
+        [GeneratedRegex(@"\d*(?=\sblue)")]
+        private static partial Regex RBlue();
+
+        [GeneratedRegex(@"(?<=Game\s)\d*")]
+        private static partial Regex RGame();
+
+        [GeneratedRegex(@"\d*(?=\sgreen)")]
+        private static partial Regex RGreen();
+
+        [GeneratedRegex(@"\d*(?=\sred)")]
+        private static partial Regex RRed();
+
+        private record GameResult(int Red, int Green, int Blue);
+
+        private record Game(int ID, List<GameResult> Results);
     }
 }
