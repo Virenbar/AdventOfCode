7e93d1658df7501a3c699e133cd5c39f962c1b43
Virenbar <rib.artem@gmail.com>
2023-12-07 11:16:57 +0500

    Add AoC 2023

---
+++ AdventOfCode2023/AdventOfCode2023/Program.cs
@@ -0,0 +1,15 @@
+﻿using AdventOfCode2023.Days;
+
+namespace AdventOfCode2023
+{
+    internal class Program
+    {
+        private static void Main()
+        {
+            BaseDay Day = new Day00();
+            var (PartOne, PartTwo) = Day.Solve();
+            Console.WriteLine(PartOne);
+            Console.WriteLine(PartTwo);
+        }
+    }
+}
