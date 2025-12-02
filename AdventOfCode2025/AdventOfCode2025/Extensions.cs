6414f1b976ed0c01f9af789f50a35804dc4e7eab
Virenbar <rib.artem@gmail.com>
2025-12-02 13:37:04 +0500

    Add AoC 2025

---
+++ AdventOfCode2025/AdventOfCode2025/Extensions.cs
@@ -0,0 +1,26 @@
+﻿namespace AdventOfCode2025
+{
+    public static class Extensions
+    {
+        public static bool IsAllUnique(this IEnumerable<char> chars)
+        {
+            var Chars = new HashSet<char>();
+            foreach (var Char in chars)
+            {
+                if (Chars.Contains(Char)) { return false; }
+                else { Chars.Add(Char); }
+            }
+            return true;
+        }
+
+        public static List<string> SplitBlocksToList(this string input) => input.Split(new[] { "\r\n\r\n", "\r\r", "\n\n" }, StringSplitOptions.None).ToList();
+
+        public static List<string> SplitToList(this string input) => input.Split(new[] { "\r\n", "\r", "\n" }, StringSplitOptions.None).ToList();
+
+        public static List<int> ToIntList(this string input) => input.Split(',').Select(int.Parse).ToList();
+
+        public static List<int> ToIntList(this List<string> input) => input.Select(int.Parse).ToList();
+
+        public static List<long> ToLongList(this List<string> input) => input.Select(long.Parse).ToList();
+    }
+}
---
+++ AdventOfCode2025/AdventOfCode2025/Program.cs
@@ -0,0 +1,15 @@
+﻿using AdventOfCode2025.Days;
+
+namespace AdventOfCode2025
+{
+    internal class Program
+    {
+        private static void Main()
+        {
+            BaseDay Day = new Day00();
+            var (PartOne, PartTwo) = Day.Solve();
+            Console.WriteLine(PartOne);
+            Console.WriteLine(PartTwo);
+        }
+    }
+}
