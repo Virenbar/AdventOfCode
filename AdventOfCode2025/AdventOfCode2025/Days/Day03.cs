253bd6b2e542e41d652781af359babc6d6341f63
Virenbar <rib.artem@gmail.com>
2025-12-05 09:40:28 +0500

    Day 3

--- AdventOfCode2025/AdventOfCode2025/Days/Day03.cs
+++ AdventOfCode2025/AdventOfCode2025/Days/Day03.cs
@@ -21,25 +21,74 @@
         protected override bool TestPartOne()
         {
             var R = CalculatePartOne(LinesTest);
-            return R == 0;
+            return R == 357;
         }
 
         protected override bool TestPartTwo()
         {
             var R = CalculatePartTwo(LinesTest);
-            return R == 0;
+            return R == 3121910778619;
         }
 
         #endregion Overrides
 
-        private static int CalculatePartOne(List<string> _)
+        private static int CalculatePartOne(List<string> batteries)
         {
-            return 0;
+            var Batteries = new Batteries(batteries);
+            return Batteries.FindMax();
         }
 
-        private static int CalculatePartTwo(List<string> _)
+        private static long CalculatePartTwo(List<string> batteries)
         {
-            return 0;
+            var Batteries = new Batteries(batteries);
+            return Batteries.FindMaxOverride();
+        }
+
+        private class Batteries
+        {
+            private readonly int[][] Banks;
+
+            public Batteries(List<string> banks)
+            {
+                Banks = banks.Select(B => B.Select(R => int.Parse(R.ToString())).ToArray()).ToArray();
+            }
+
+            public int FindMax()
+            {
+                return Banks.Select(B =>
+                   {
+                       var max = B[..^1].Max();
+                       var maxFirst = Array.IndexOf(B, max);
+                       var maxSecond = B[(maxFirst + 1)..].Max();
+                       var number = (max * 10) + maxSecond;
+                       return number;
+                   }).Sum();
+            }
+
+            public long FindMaxOverride()
+            {
+                return Banks.Select(B =>
+                    {
+                        var list = B.ToList();
+                        while (list.Count > 12)
+                        {
+                            var minFound = false;
+                            for (var i = 0; !minFound && i < list.Count - 1; i++)
+                            {
+                                if (list[i] < list[i + 1])
+                                {
+                                    minFound = true;
+                                    list.RemoveAt(i);
+                                }
+                            }
+
+                            if (!minFound)
+                                list.Remove(list.Min());
+                        }
+
+                        return long.Parse(string.Join("", list));
+                    }).Sum();
+            }
         }
     }
 }
